3fd2cb99db4c5da46a0f6cbbd2807cd7c49b80a5
Rory Monaghan <Rorymon@gmail.com>
2016-09-27 12:21:34 -0700

    removed number

--- PowerShell/query65usersandmessage.ps1
+++ PowerShell/query65usersandmessage.ps1
@@ -26,22 +26,17 @@ $ConnectSession = Get-XASession -BrowserName connect | Where-Object { $_.Account
 if ($ConnectSession) {
 $row = New-Object System.Object
 $row | Add-Member -MemberType NoteProperty -Name "User" -Value $user # create a property called UserID. This will be the UserID column
-Get-XASession | Where-Object { $_.AccountName -match $user } | Send-XASessionMessage -MessageTitle "Attn: Staff Members" -MessageBody "You are receiving this notification because the new Connect-PRD icon 
+Get-XASession | Where-Object { $_.AccountName -match $user } | Send-XASessionMessage -MessageTitle "Attn: Staff Members" -MessageBody "You are receiving this notification because the new Connect icon 
 has been published to your facility and your login.
 
-PLEASE begin using the Connect-PRD icon.  If you experience any issues 
-with the new icon, call the Service Desk at 480-882-4357, and continue to use the Connect icon.
+PLEASE begin using the Connect icon.  If you experience any issues 
+with the new icon, call the Service Desk at <>.
 
-If you have not received the new Connect-PRD icon, please call the Service Desk at 480-882-4357, 
+If you have not received the new Connect icon, please call the Service Desk at <>, 
 and continue to use the Connect icon.
-
-If you are a staff member who uses a eSignature pad workstation, a recent fix was distributed to the PC’s. 
-If you are continuing to experience eSig related issue with Connect-PRD, please call the Service Desk at 480-882-4357 to
-report the error. 
  
-
 Best Regards, 
-HonorHealth Citrix Team"
+Citrix Team"
 $row | Add-Member -MemberType NoteProperty -Name "User" -Value $user -force
 #$row | Add-Member -MemberType NoteProperty -Name "Status" -Value # create a property called UserID. This will be the UserID column
 #$row | Add-Member -MemberType NoteProperty -Name "Status" -Value "Notified" # create a property called UserID. This will be the UserID column
